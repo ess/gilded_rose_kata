8b17ff69abae66344c7cab2c6ddb326bfa83b0b5
Dennis Walters <dwalters@engineyard.com>
2015-12-21 11:18:06 -0500

    All the little things
    Following along with Sandi's talk on the subject, step by step,
    to see how a pure OO approach feels.

--- Gemfile
+++ Gemfile
@@ -1,3 +1,4 @@
 source 'https://rubygems.org'
 gem 'rspec'
 gem 'rspec-given'
+gem 'minitest', '~> 4.7'
--- gilded_rose.rb
+++ gilded_rose.rb
@@ -1,63 +1,94 @@
+module GildedRose
+  class Item
+    attr_reader :quality, :days_remaining
+
+    def initialize(quality, days_remaining)
+      @quality, @days_remaining = quality, days_remaining
+    end
+
+    def tick
+    end
+  end
+
+  class Normal < Item
+    def tick
+      @days_remaining -= 1
+      return if @quality == 0
+
+      @quality -= 1
+      @quality -= 1 if @days_remaining <= 0
+    end
+  end
+
+  class Brie < Item
+    def tick
+      @days_remaining -= 1
+      return if @quality >= 50
+
+      @quality += 1
+      return if @quality == 50
+      @quality += 1 if @days_remaining <= 0
+    end
+  end
+
+  class Backstage < Item
+    def tick
+      @days_remaining -= 1
+      return if @quality >= 50
+      return @quality = 0 if @days_remaining < 0
+
+      @quality += 1
+      @quality += 1 if @days_remaining < 10
+      @quality += 1 if @days_remaining < 5
+    end
+  end
+
+  class Conjured < Item
+    def tick
+      @days_remaining -= 1
+      return if @quality == 0
+
+      @quality -= 2
+      @quality -= 2 if @days_remaining <= 0
+    end
+  end
+
+  DEFAULT_CLASS = Item
+  SPECIALIZED_CLASSES = {
+    'normal' => Normal,
+    'Aged Brie' => Brie,
+    'Backstage passes to a TAFKAL80ETC concert' => Backstage,
+    'Conjured Mana Cake' => Conjured
+  }
+
+  def self.for(name, days_remaining, quality)
+    (SPECIALIZED_CLASSES[name] || DEFAULT_CLASS).
+      new(quality, days_remaining)
+  end
+
+end
+
 def update_quality(items)
   items.each do |item|
-    if item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert'
-      if item.quality > 0
-        if item.name != 'Sulfuras, Hand of Ragnaros'
-          item.quality -= 1
-        end
-      end
-    else
-      if item.quality < 50
-        item.quality += 1
-        if item.name == 'Backstage passes to a TAFKAL80ETC concert'
-          if item.sell_in < 11
-            if item.quality < 50
-              item.quality += 1
-            end
-          end
-          if item.sell_in < 6
-            if item.quality < 50
-              item.quality += 1
-            end
-          end
-        end
-      end
-    end
-    if item.name != 'Sulfuras, Hand of Ragnaros'
-      item.sell_in -= 1
-    end
-    if item.sell_in < 0
-      if item.name != "Aged Brie"
-        if item.name != 'Backstage passes to a TAFKAL80ETC concert'
-          if item.quality > 0
-            if item.name != 'Sulfuras, Hand of Ragnaros'
-              item.quality -= 1
-            end
-          end
-        else
-          item.quality = item.quality - item.quality
-        end
-      else
-        if item.quality < 50
-          item.quality += 1
-        end
-      end
-    end
+    gr = GildedRose.for(item.name, item.sell_in, item.quality)
+    gr.tick
+    item.quality = gr.quality
+    item.sell_in = gr.days_remaining
   end
 end
 
-# DO NOT CHANGE THINGS BELOW -----------------------------------------
+## DO NOT CHANGE THINGS BELOW -----------------------------------------
 
 Item = Struct.new(:name, :sell_in, :quality)
 
-# We use the setup in the spec rather than the following for testing.
-#
-# Items = [
-#   Item.new("+5 Dexterity Vest", 10, 20),
-#   Item.new("Aged Brie", 2, 0),
-#   Item.new("Elixir of the Mongoose", 5, 7),
-#   Item.new("Sulfuras, Hand of Ragnaros", 0, 80),
-#   Item.new("Backstage passes to a TAFKAL80ETC concert", 15, 20),
-#   Item.new("Conjured Mana Cake", 3, 6),
-# ]
+## We use the setup in the spec rather than the following for testing.
+##
+## Items = [
+##   Item.new("+5 Dexterity Vest", 10, 20),
+##   Item.new("Aged Brie", 2, 0),
+##   Item.new("Elixir of the Mongoose", 5, 7),
+##   Item.new("Sulfuras, Hand of Ragnaros", 0, 80),
+##   Item.new("Backstage passes to a TAFKAL80ETC concert", 15, 20),
+##   Item.new("Conjured Mana Cake", 3, 6),
+## ]
 
